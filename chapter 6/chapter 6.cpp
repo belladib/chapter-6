965aa200bfdfa9503c42c2b34a3a5cff82bece6e
baebe <baebe@FELICITY>
2024-11-04 22:34:15 -0500

    YAY

--- chapter 6/chapter 6.cpp
+++ chapter 6/chapter 6.cpp
@@ -1,20 +1,85 @@
 // chapter 6.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//determines which region is safest based on number of accidents
 
 #include <iostream>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+//prototypes
+void find(double, double, double, double);
+static void Print(double, double, double, double, double);
+
+double north, south, west, east, smallest, largest; //used in all parts of the program
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+
+
+    cout << "How many accidents happened in the northern part of your city?\n";
+    cin >> north;
+    if (north < 0)
+    {
+        cout << "Invalid, please enter a number that is at least 0";
+        return 1;
+    }
+
+    cout << "\nHow many accidents happened in the southern part of your city?\n";
+    cin >> south;
+    if (south < 0)
+    {
+        cout << "Invalid, please enter a number that is at least 0";
+        return 1;
+    }
+
+    cout << "\nHow many accidents happened in the east part of your city?\n";
+    cin >> east;
+    if (east < 0) 
+    {
+        cout << "Invalid, please enter a number that is at least 0";
+        return 1;
+    }
+    cout << "\nHow many accidents happened in the west part of your city?\n";
+    cin >> west; 
+    if (west < 0)
+    {
+        cout << "Invalid, please enter a number that is at least 0";
+        return 1;
+    }
+    find(north, south, east, west);
+
+    cout << "\nThe safest region with the least amount of accidents was ";
+    Print(smallest, east, west, south, north);
+    cout << " with " << smallest << " accidents\n";
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void find(double north, double south, double east, double west)
+{
+    if (north < east && north < west && north < south) //compares north
+        smallest = north;
+    else if (east < north && east < south && east < west) // compares east
+        smallest = east;
+    else if (south < east && south < west && south < north)// compares south
+        smallest = south;
+    else if (west < east && west < north && west < south)// compares west
+        smallest = west;
+    return;
+
+    
+}
+static void Print(double smallest, double east, double west, double south, double north)
+{// so that it will know which region to print
+    if (smallest == east)
+        cout << "east";
+    else if (smallest == west)
+        cout << "west";
+    else if (smallest == north)
+        cout << "north";
+    else if (smallest == south)
+        cout << "south";
+} 
+
+
+
