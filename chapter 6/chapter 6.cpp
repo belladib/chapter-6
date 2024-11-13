670fe92c9ed00352199b6af9815885406ac25bb4
baebe <baebe@FELICITY>
2024-11-12 22:09:28 -0500

    WOOHOO

--- chapter 6/chapter 6.cpp
+++ chapter 6/chapter 6.cpp
@@ -1,4 +1,4 @@
-// chapter 6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//AVERAGE OF 5 SCORES
 //
 
 #include <iostream>
@@ -6,19 +6,63 @@
 #include <iomanip>
 #include <cmath>
 using namespace std;
-int main()
+double smallest, largest, average, sum;
+
+void findsmall(double score1, double score2, double score3, double score4, double score5)//FINDS SMALLEST VALUE
+{
+    if (score1 < score2 && score1 < score3 && score1 < score4 && score1 < score5) 
+        smallest = score1;
+    else if (score2 < score1 && score2 < score3 && score2 < score4 && score2 < score5) 
+        smallest = score2;
+    else if (score3 < score2 && score3 < score1 && score3 < score4 && score3 < score5)
+        smallest = score3;
+    else if (score4 < score2 && score4 < score3 && score4 < score1 && score4 < score5)
+        smallest = score4;
+    else if (score5 < score2 && score5 < score3 && score5 < score4 && score5 < score1)
+        smallest = score5;
+    return;
+}
+
+void findbig(double score1, double score2, double score3, double score4, double score5)//FINDS LARGEST VALUE
+{
+    if (score1 > score2 && score1 > score3 && score1 > score4 && score1 > score5)
+        largest = score1;
+    else if (score2 > score1 && score2 > score3 && score2 > score4 && score2 > score5) 
+        largest = score2;
+    else if (score3 > score2 && score3 > score1 && score3 > score4 && score3 > score5)
+        largest = score3;
+    else if (score4 > score2 && score4 > score3 && score4 > score1 && score4 > score5)
+        largest = score4;
+    else if (score5 > score2 && score5 > score3 && score5 > score4 && score5 > score1)
+        largest = score5;
+    return;
+}
+
+void math(double score1, double score2, double score3, double score4, double score5) //CALCULATION
+{
+    findsmall(score1, score2, score3, score4, score5);
+    findbig(score1, score2, score3, score4, score5);
+    sum = score1 + score2 + score3 + score4 + score5;
+    sum -= smallest;
+        sum -= largest;
+        average = sum / 3;
+}
+
+
+int main() //MAIN PROGRAM
 {
     double score1, score2, score3, score4, score5;
 
 
-    cout << "Please enter your 5 scores for the contestant, must be 1 - 10";
+    cout << "Please enter your 5 scores for the contestant, must be 1 - 10\n";
     cin >> score1 >> score2 >> score3 >> score4 >> score5;
     if (score1 < 0 || score1 > 10 || score2 < 0 || score2 > 10 || score3 < 0 || score3 > 10 || score4 < 0 || score4 > 10 || score5 < 0 || score5 > 10)
     {
-        cout << "Invalid, must be between 0 and 10.";
+        cout << "\nInvalid, must be between 0 and 10.";
             return 1;
     }
-
+    math(score1, score2, score3, score4, score5);
+    cout << showpoint << fixed << setprecision(2) << "\nThe average score without the lowest and highest scores is " << average << endl;
 
 
 
